089430b34ca8c30132c40127ca481c6f54412390
Timo Bechtel <48655057+TimoBechtel@users.noreply.github.com>
2026-05-19 07:32:34 +0000

    Update cmux Chromium cask to 0.64.6-chromium.58

--- Casks/cmux-chromium.rb
+++ Casks/cmux-chromium.rb
@@ -1,6 +1,6 @@
 cask "cmux-chromium" do
-  version "0.64.6-chromium.57"
-  sha256 "f34504b0af1332ccd8ed9e6ede02017b5f84b9b592fa3b5e68bdb8efdb246abe"
+  version "0.64.6-chromium.58"
+  sha256 "c7bc1ed6fdf6d8f54b7e878f8fea8d5e06944c27c884a6e25ec34001fc653c23"
 
   url "https://github.com/TimoBechtel/cmux/releases/download/chromium-#{version}/cmux-chromium-macos-arm64.zip"
   name "cmux Chromium"
